a3bc2b3e315b54046ca9ad2cf854a49bb1bdd16c
Aspire VX 15 <Aspire VX 15@DESKTOP-PFLGDUI>
2018-07-31 11:14:34 +0700

    Myweb1

---
+++ Myweb1/src/Person.java
@@ -0,0 +1,19 @@
+
+public class Person {
+   private String name;
+   private String latsname;
+   
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getLatsname() {
+	return latsname;
+}
+public void setLatsname(String latsname) {
+	this.latsname = latsname;
+}
+   
+}
---
+++ Myweb1/src/RunPerson.java
@@ -0,0 +1,10 @@
+
+public class RunPerson {
+
+	public static void main(String[] args) {
+		Person p = new Person();
+		
+
+	}
+
+}
